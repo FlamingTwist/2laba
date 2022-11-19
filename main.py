66695c855d22bcf2f78352e212b2b7b05c428f3a
FlamingTwist <r.pavel.d@mail.ru>
2022-11-19 11:48:09 +0300

    Weather

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+city = "Moscow,RU"
+# мой api ключ пока не активирован, сказали подождать пару часов
+appid = "bfe66093d053f44991f03596042e278f"
+
+# отправляем запрос с нужными параметрами
+res = requests.get("http://api.openweathermap.org/data/2.5/weather", params = {'q':city, 'units':'metric', 'lang':'ru', 'APPID':appid} )
+
+
+data = res.json()
+
+print(data)
+
